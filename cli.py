31e35e47f9a113220bcb5a6812d158a54bef0984
Geulmaster <eyal.geulayev@gmail.com>
2021-05-07 15:56:15 +0300

    Add a TODO for future working on the project

--- cli.py
+++ cli.py
@@ -1,7 +1,14 @@
 import pyshark
 
-capture = pyshark.LiveCapture(interface='Ethernet')
+capture = pyshark.LiveCapture(interface='enp0s3')
 
-for packet in capture.sniff_continuously(packet_count=1):
-    print("source: ", packet["ip"].src)
-    print("destination: ", packet["ip"].dst)
+packets_list = capture.sniff_continuously(packet_count=5)
+
+for index, packet in enumerate(packets_list):
+    try:
+        print("source: ", packet["ip"].src)
+        print("destination: ", packet["ip"].dst)
+    except:
+        print(f"Packet {index} do not have an IP layer")
+
+# TODO: cli that gets interface, number of packets, an option to return other values than IPs
